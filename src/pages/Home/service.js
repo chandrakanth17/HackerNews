a8f129b1ac13a38e955ee799ca43d687c1b3f2e2
chandrakanth <chandrakanth.n@greyamp.com>
2020-04-27 14:23:21 +0530

    removed cache adapter

--- src/pages/Home/service.js
+++ src/pages/Home/service.js
@@ -1,20 +1,12 @@
 import axios from 'axios';
 import { INITIAL_LENGTH } from './Constant';
-import { setupCache } from 'axios-cache-adapter';
 
-const cache = setupCache({
-  maxAge: 15 * 60 * 1000
-});
-
-// Create `axios` instance passing the newly created `cache.adapter`
-const api = axios.create({
-  adapter: cache.adapter
-});
 export const getNewsFeed = (pageNumber = INITIAL_LENGTH) => {
-  return api({
-    url: `http://hn.algolia.com/api/v1/search?tags=front_page&page=${pageNumber}`,
-    method: 'get'
-  })
+  return axios
+    .request({
+      url: `http://hn.algolia.com/api/v1/search?tags=front_page&page=${pageNumber}`,
+      method: 'get'
+    })
     .then(response => {
       return response;
     })
