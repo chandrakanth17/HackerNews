1639f3dec4a2fa84f04da8f24b91ccf089e4e258
chandrakanth <chandrakanth.n@greyamp.com>
2020-04-27 15:52:26 +0530

    fixing issue with prod failure

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router';
 import 'babel-polyfill';
 import Home from './Home';
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
