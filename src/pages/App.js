b5f69869ea0cbe9c0c110690cc9980df34f73fd8
chandrakanth <chandrakanth.n@greyamp.com>
2020-04-27 16:32:36 +0530

    Build fix'

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Switch, Route, Router } from 'react-router';
+import { Switch, Route } from 'react-router';
 import 'babel-polyfill';
 import Home from './Home';
-import { createBrowserHistory } from 'history';
-const history = createBrowserHistory();
 
 class App extends React.Component {
   render() {
     return (
-      <Router history={history}>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
     );
   }
 }
